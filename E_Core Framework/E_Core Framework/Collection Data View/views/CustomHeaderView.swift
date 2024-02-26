bda25844fbaa7c177c9eb4cda1bc5cf760bd1270
Radwa Mohmed <78206765+RadwaMohamed29@users.noreply.github.com>
2024-02-26 13:10:57 +0200

    finished table view custom header

--- E_Core Framework/E_Core Framework/Collection Data View/views/CustomHeaderView.swift
+++ E_Core Framework/E_Core Framework/Collection Data View/views/CustomHeaderView.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class CustomHeaderView: UITableViewHeaderFooterView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
+    override func awakeFromNib() {
+           super.awakeFromNib()
+           print("custom header")
+       }
+    
+    override init(reuseIdentifier: String?) {
+           super.init(reuseIdentifier: reuseIdentifier)
+       }
+
+       required init?(coder: NSCoder) {
+           super.init(coder: coder)
+       }
 }
