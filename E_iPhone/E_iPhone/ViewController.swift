e8ca279df1c99cddc50d5d5be5d4e0a532684c33
Aalaa Eid <aalaaeiid@gmail.com>
2024-03-03 12:16:58 +0200

    dublicate config

--- E_iPhone/E_iPhone/ViewController.swift
+++ E_iPhone/E_iPhone/ViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 import UIKit
 import E_Core_Framework
+
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let eCoreUIComponent = TreeVC()
-//        E_Core_UI_ComponentVC(
-//            message: "Sum of 1+2+3 = \(E_Core_sum.sum(numbers: [1,2,3])) calculated using E_Core_sum..sum function",
-//            btnTitle: "good",
-//            btnColor: .red
-//        )
-//        eCoreUIComponent.btnHandler = {
-//            self.showAlert()
-//        }
-        addChild(eCoreUIComponent)
-        view.addSubview(eCoreUIComponent.view)
-        eCoreUIComponent.didMove(toParent: self)
+
+        
+
+            // Accessing values from Info.plist
+            if let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+                print("App Name: \(appName)")
+                if appName == CoreName.release.rawValue {
+                    let search = SearchTableView(numberOfRows: 3, subject: ["موضوع بحث","موضوع هام","موضوع للعلم"], attachment: ["1","2","3"])
+                    addChild(search)
+                    view.addSubview(search.view)
+                    search.didMove(toParent: self)
+                } else if appName == CoreName.debug.rawValue {
+                    
+                    let coreUI = SearchTableView(numberOfRows: 3, subject: ["موضوع بحث","موضوع هام","موضوع للعلم"], attachment: ["1","2","3"])
+                    addChild(coreUI)
+                    view.addSubview(coreUI.view)
+                    coreUI.didMove(toParent: self)
+                    
+                } else {
+                    let coreUI = SearchTableView(numberOfRows: 3, subject: ["موضوع بحث","موضوع هام","موضوع للعلم"], attachment: ["1","2","3"])
+                    addChild(coreUI)
+                    view.addSubview(coreUI.view)
+                    coreUI.didMove(toParent: self)
+                }
+            }
+
+                
+        
+     
+
     }
     
     func showAlert()  {
